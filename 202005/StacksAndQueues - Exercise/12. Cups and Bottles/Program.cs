54187d2b01bde2f8a57e3ab9d1fa597f9e1697ad
Tsvetan Borisov <tsborissov@gmail.com>
2020-09-08 21:09:54 +0300

    Stacks & Queues - Exercise

---
+++ 202005/StacksAndQueues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> cups = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            int wastedWater = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                int currentCup = cups.Peek();
+
+                while (bottles.Any())
+                {
+                    int currentBottle = bottles.Pop();
+
+                    currentCup -= currentBottle;
+
+                    if (currentCup <= 0)
+                    {
+                        cups.Dequeue();
+                        wastedWater += Math.Abs(currentCup);
+                        break;
+                    }
+                }
+            }
+
+            if (!cups.Any())
+            {
+                Console.WriteLine($"Bottles: {string.Join(' ', bottles)}");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(' ', cups)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
---
+++ 202005/StacksAndQueues - Exercise/7. Truck Tour/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int pumpsCount = int.Parse(Console.ReadLine());
+
+            Queue<string> pumps = new Queue<string>();
+
+            for (int i = 0; i < pumpsCount; i++)
+            {
+                pumps.Enqueue(Console.ReadLine());
+            }
+
+            int firstPointIndex = 0;
+            bool isTripCompleted = false;
+
+            for (int i = 0; i < pumpsCount; i++)
+            {
+                int petrolTotal = 0;
+
+                string[] pumpsTempArr = pumps.ToArray();
+
+                for (int j = 0; j < pumpsCount; j++)
+                {
+                    string currentPump = pumpsTempArr[j];
+                    int currentPetrol = int.Parse(currentPump.Split().ToArray()[0]);
+                    int distance = int.Parse(currentPump.Split().ToArray()[1]);
+
+                    petrolTotal += currentPetrol;
+
+                    if (petrolTotal < distance)
+                    {
+                        isTripCompleted = false;
+                        petrolTotal = 0;
+                        break;
+                    }
+                    else
+                    {
+                        isTripCompleted = true;
+                        petrolTotal -= distance;
+                    }
+                }
+
+                if (isTripCompleted)
+                {
+                    firstPointIndex = i;
+                    break;
+                }
+                
+
+                pumps.Enqueue(pumps.Dequeue());
+            }
+
+            Console.WriteLine(firstPointIndex);
+        }
+    }
+}
